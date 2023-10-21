0776482a075ad30a579e969d27eec6bdd3704fb5
kiprella <kondreckas.kipras@gmail.com>
2023-10-21 17:15:20 +0300

    VIX test added

--- ADF test/engine.py
+++ ADF test/engine.py
@@ -10,9 +10,9 @@ data = pd.read_csv('./data/'+test)
 
 # Ignore NaN values 
 prices = data['Close'].dropna()
-
-# ADF test
 result = adfuller(prices)
+
+
 
 print(f'ADF Statistic: {result[0]}')
 
@@ -27,5 +27,17 @@ if result[0] < result[4]['5%']:
 else: 
     print("Series is likely non-stationary")
 
+# USDCAD.csv is not stationary
+# Enter dataset name: USDCAD.csv
+# ADF Statistic: -1.6536349919287192
+# p-value: 0.4551453647528598
+# Critical Values: {'1%': -3.431603356058229, '5%': -2.862093852984781, '10%': -2.5670648157322997}
+# Series is likely non-stationary
+# ADF statistic > 5% critical value, suggesting that the series is non-stationary
 
 
+# Enter dataset name: VIX.csv
+# ADF Statistic: -6.892035570356753
+# p-value: 1.3479540617342404e-09
+# Critical Values: {'1%': -3.4311190188723706, '5%': -2.861879854023006, '10%': -2.566950899393073}
+# Series is likely stationary
